ce2ab6905ca04e164afcdaa39ca51dfc7f57a5cc
aliakbarzohour <aliakbar.zohour@gmail.com>
2024-06-26 03:18:42 +0330

    better styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import ToastifyNotification from './components/ToastifyNotification';
 function App() {
 
   return (
-    <>
+    <div className='flex justify-center items-center w-full h-screen'>
       <button
-        className="btn-primary"
+        className="py-3 px-8 rounded-xl text-center bg-orange-600 text-white"
         onClick={() => toast(<ToastifyNotification title="پیام جدییید" body="اگه این پیامو دیدین , خوشحال بشید  چون داره کار میکنه" />)}
       >
         نشون دادن نوتیف
       </button>
 
       <ToastContainer hideProgressBar />
-    </>
+    </div>
   )
 }
 
--- src/components/ToastifyNotification.jsx
+++ src/components/ToastifyNotification.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const ToastifyNotification = ({ title, body }) => {
     return (
-        <div className="push-notification">
-            <h2 className="push-notification-title">{title}</h2>
-            <p className="push-notification-text">{body}</p>
-        </div>
+        <motion.div
+            initial={{ y: '-3rem' }}
+            animate={{ y: 0 }}
+            className="push-notification absolute top-5 right-5 rounded-xl shadow-xl shadow-gray-200 border border-gray-300 p-4 bg-white flex flex-col justify-end items-end">
+            <h2 className="push-notification-title font-semibold text-lg">{title}</h2>
+            <p className="push-notification-text font-normal text-gray-600">{body}</p>
+        </motion.div>
     )
 }
 export default ToastifyNotification
